a1901ab416365489a16462c9c6c013f6703c8065
vlad1xxx <vlad1x2807@gmail.com>
2024-04-14 19:04:05 +0300

    solution 3

--- data/users.py
+++ data/users.py
@@ -2,6 +2,7 @@ import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
@@ -17,6 +18,13 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
 
 
 class Jobs(SqlAlchemyBase):
--- main.py
+++ main.py
@@ -1,18 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
-from data.users import Jobs, User
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, TextAreaField, EmailField, StringField
+from wtforms.validators import DataRequired
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
+class RegisterForm(FlaskForm):
+    login = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    re_password = PasswordField('Password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def main():
-    db_session.global_init('db/blogs.db')
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
-    return render_template('index.html', jobs=jobs)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.re_password.data:
+            return render_template('index.html', title='Register form',
+                                   form=form,
+                                   message='Пароли не совпадают')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            return render_template('index.html', title='Регистрация',
+                                   form=form,
+                                   message='Такой пользователь уже есть')
+        user = User(
+            email=form.login.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/complete')
+    return render_template('index.html', title='Регистрация', form=form)
+
+
+@app.route('/complete')
+def complete():
+    return render_template('complete.html', title='Регистрация')
 
 
 if __name__ == '__main__':
+    db_session.global_init('db/blogs.db')
     app.run(port=8000, host='127.0.0.1')
